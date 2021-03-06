caf5b0470cd8bc06f8e47d2ba08780904c9d0e76
Mauricio <maubot01@gmail.com>
2021-03-06 19:26:23 -0400

    Jumbotron container

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
-import jumboData from "./fixtures/jumbo.json";
-import Jumbotron from "./components/jumbotron";
+import JumbotronContainer from "./containers/jumbotron";
 
 function App() {
-  return (
-    <div className="App">
-      <Jumbotron.Container>
-        {jumboData.map((item) => (
-          <Jumbotron key={item.id} direction={item.direction}>
-            <Jumbotron.Pane>
-              <Jumbotron.Title>{item.title}</Jumbotron.Title>
-              <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
-            </Jumbotron.Pane>
-            <Jumbotron.Image src={item.image} alt={item.alt} />
-          </Jumbotron>
-        ))}
-      </Jumbotron.Container>
-    </div>
-  );
+  return <JumbotronContainer />;
 }
 
 export default App;
