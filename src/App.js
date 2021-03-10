f82783987ba83e76a1f64bb53c5478b47113859c
Mauricio <maubot01@gmail.com>
2021-03-10 13:08:08 -0400

    home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import JumbotronContainer from "./containers/jumbotron";
-import FooterContainer from "./containers/footer";
-import FaqsContainer from "./containers/faqs";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
+import Browse from "./pages/browse";
+import Home from "./pages/home";
+import SignIn from "./pages/signin";
+import SignUp from "./pages/signup";
 
 function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
-    </>
+    <Router>
+      <Switch>
+        <Route exact path={ROUTES.HOME}>
+          <Home />
+        </Route>
+        <Route exact path={ROUTES.BROWSE}>
+          <Browse />
+        </Route>
+        <Route exact path={ROUTES.SIGN_IN}>
+          <SignIn />
+        </Route>
+        <Route exact path={ROUTES.SIGN_UP}>
+          <SignUp />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
